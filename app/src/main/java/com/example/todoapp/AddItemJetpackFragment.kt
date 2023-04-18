7eeee3708df156c8ea60bee602498321d1c4b93c
trongan123 <62749635+trongan123@users.noreply.github.com>
2023-04-18 15:04:28 +0700

    commit add and update item

---
+++ app/src/main/java/com/example/todoapp/AddItemJetpackFragment.kt
@@ -0,0 +1,49 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.todoapp.model.TodoItem
+import com.example.todoapp.viewmodel.AddItemFragmentViewModal
+
+class AddItemJetpackFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        var viewModel: AddItemFragmentViewModal =
+            ViewModelProvider(requireActivity()).get(AddItemFragmentViewModal::class.java)
+        return ComposeView(requireContext()).apply {
+            titleItem = ""
+            createdDateItem =""
+            completedDateItem =""
+            statusItem =""
+            descriptionItem= ""
+            todoItem = TodoItem()
+            setContent {
+                Column(modifier = Modifier.padding(30.dp)) {
+                    Texttitle()
+                    CommonSpace()
+                    TextDescription()
+                    CommonSpace()
+                    TextCreatedDate()
+                    CommonSpace()
+                    TextCompletedDate()
+                    CommonSpace()
+                    dropDownMenuStatus()
+
+                }
+                LayoutAddButton(viewModel, requireView())
+
+            }
+
+        }
+    }
+}
